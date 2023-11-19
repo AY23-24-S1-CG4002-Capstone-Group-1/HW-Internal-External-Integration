dfb802f5c0aa68156286cf4a752b7b4288a0956c
Wilson Ng <wilsonngja@gmail.com>
2023-11-19 11:34:14 +0800

    Change to the correct file

--- Arduino Code/Bluno_Glove_Data_Transfer/Bluno_Glove_Data_Transfer.ino
+++ Arduino Code/Bluno_Glove_Data_Transfer/Bluno_Glove_Data_Transfer.ino
@@ -2,7 +2,21 @@
 #include "CRC8.h"
 #include "CRC.h"
 #include "Bluno_Variables.h"
+//#include "start_of_move.h"
 #include <Adafruit_MPU6050.h>
+#include <Arduino.h>
+#include <arduinoFFT.h>
+
+#define SAMPLES 16
+#define TOTAL_SAMPLES 32
+#define SAMPLING_FREQUENCY 100
+#define THRESHOLD 700
+#define TS 50
+
+arduinoFFT FFT;
+long long fft_sum =0;
+Glove_Data gloveDatas[TOTAL_SAMPLES]={};
+
 
 Adafruit_MPU6050 imu;
 
@@ -11,13 +25,10 @@ float flexMinResistance = 40000;
 float flexMaxResistance = 300000;
 float baseFlexValue = 16;
 float flexInterval = 10;
-
-
-
+String actions[11] = { "nothing", "doctor", "spider", "punch",  "thor", "nothing", "shield", "spear", "grenade", "reload", "nothing"}; 
 
 float VCC = 5;
 float flexResistor = 10000; //10K Ohms
-
 
 void setupImu() {
   if (!imu.begin()) {
@@ -38,6 +49,29 @@ void setupImu() {
   delay(10);
 }
 
+void read_action (){
+   for (int i =0; i < 11; i ++){
+     delay (1000); 
+
+     Serial.println(actions[i]); 
+     delay(500);
+     Serial.print("3"); 
+     delay(500); 
+     Serial.print("2"); 
+     delay(500); 
+     Serial.print("1"); 
+     delay(500); 
+     Serial.println("go"); 
+    
+    for (int j = 0; j< 50; j ++){
+      Serial.print(j); 
+      Serial.print(", ");
+      senddata();
+      delay(50);  //sampling rate
+    }
+   }
+}
+
 int getFlexSensorValue(const int flexPin) {
   
   int ADCRaw = analogRead(flexPin);
@@ -50,14 +84,13 @@ int getFlexSensorValue(const int flexPin) {
   return flexValue;
 }
 
+
 Glove_Data getGloveReadings() {
   
   sensors_event_t a,g, temp;
   imu.getEvent(&a, &g, &temp);
 
   Glove_Data gloveData;
-
-
 
   gloveData.GyroX = mapGyro(g.gyro.x);
   gloveData.GyroY = mapGyro(g.gyro.y);
@@ -72,7 +105,106 @@ Glove_Data getGloveReadings() {
 
   return gloveData;
 }
+void send_remaining_readings(){
+  int n = SAMPLES;
+  for (int i = SAMPLES; i < TOTAL_SAMPLES; i ++){
+    Glove_Data gloveData;
+    gloveDatas[i] = getGloveReadings(); 
+    delay(TS); 
+  }
+}
 
+bool start_of_move(){ 
+  int n = SAMPLES; 
+  double vReal[3][SAMPLES] = {}; 
+  double vImag[3][SAMPLES] = {}; 
+  
+  //collect samples
+  for (int i = 0; i < n; i ++){
+    Glove_Data gloveData;
+    gloveDatas[i] = getGloveReadings(); 
+
+    vReal[0][i] += gloveDatas[i].GyroX;
+    vReal[1][i] += gloveDatas[i].GyroY;
+
+    //vReal[0][i] += gloveDatas[i].AccX;
+    vReal[2][i] += gloveDatas[i].AccY*5;
+
+    //vReal[2][i] += gloveDatas[i].AccZ;
+    //Serial.print(vReal[0][i]); 
+    //vReal[1][i] += gloveDatas[i].AccX + gloveDatas[i].AccY + gloveDatas[i].AccZ;
+    //Serial.print(vReal[1][i]); 
+    //vReal[2][i] += gloveDatas[i].Flex1 + gloveDatas[i].Flex2;
+
+    vImag[0][i] = 0;
+    vImag[1][i] = 0;
+    vImag[2][i] = 0;
+
+    delay(TS); 
+  }
+
+  //fft
+  //float weighted_sums[3]= {}; 
+  float sums[3] = {0,0,0};
+
+  for (int j = 0; j < 3; j ++){
+    int weighted_sum = 0; 
+
+    FFT = arduinoFFT(vReal[j], vImag[j], SAMPLES, SAMPLING_FREQUENCY);
+    FFT.Windowing(vReal[j], SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal[j], vImag[j], SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal[j], vImag[j], SAMPLES);
+
+    float delta_f = SAMPLING_FREQUENCY / SAMPLES;
+
+    //weighted sum of frequency*amplitude
+    for (int i = 0; i < SAMPLES / 2; i++) {
+      int frequency = round(i * delta_f);
+      long amplitude = round(vReal[j][i]);
+      int scale = 2;
+      if (j ==0){
+        scale = 1.2;
+      }
+      if (j == 1){
+        scale = 1;
+      }
+      sums[j] += (scale*frequency * amplitude)/100;
+    }
+    //Serial.print(sums[j]);
+    //Serial.print(", ");
+
+    if (sums[j]> 400){
+      return true;
+    }
+   // sums[j] += weighted_sum;
+    }
+  //int sum = weighted_sum;
+  //Serial.println();
+
+  if (sums[1] > 250){
+    if (sums[0] >200 || sums[3]> 150){
+      return true;
+    }
+  }
+
+  if (sums[0] > 300){
+    if (sums[1] > 200 || sums[2] > 200){
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void senddata(){
+    struct Data_Packet data_packet;
+
+    for (int i = 0; i < TOTAL_SAMPLES; i++){
+      data_packet = computeDataPacketResponse(gloveDatas[i]);
+      Serial.write((uint8_t*) &data_packet, sizeof(data_packet));
+      delay(8);
+    }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -81,73 +213,24 @@ void setup() {
   pinMode(FLEX_PIN1, INPUT);
   pinMode(FLEX_PIN2, INPUT);
 
-  Serial.println("GGWP");
   setupImu();  
-  Serial.println("GGWP LIAO");
 }
 
 
 void loop() {
-  struct Hello_Packet hello_packet_response;
-  // Serial.print("LOL");
-  // Check for data available
-  while (Serial.available()) {
-    if (index != 20) {
-      byte data = Serial.read();
-      incomingData[index] = data;
-//      Serial.print(data);
-      index += 1;
-    } 
-  }
-  
-  
+  //Serial.println("start");
 
+  if (start_of_move()){
+    //Serial.println("Move Started");
+    //sdelay(1000);
+    send_remaining_readings();
+    senddata();
+  }
+}
     
-    if (calculateCRC8(incomingData, 20) == 0){
+    
+
       
+    
       
-      // Sending Hello Packet
-      if ((incomingData[PACKET_ID_INDEX] == HELLO_PACKET_ID) || (incomingData[PACKET_ID_INDEX] == CONN_EST_PACKET_ID)){
-        // Hello Packet is correctly computed, hence write a response to send back to Relay Node.
-        hello_packet_response = computeHelloPacketResponse();
-
-
-        // If previous connected and Arduino is not powered off, then restart manually
-        if ((hasReceivedHelloPacket == true) && (hasReceivedConnPacket)) {
-          hasReceivedHelloPacket = false;
-          hasReceivedConnPacket = false;
-          isReadyToSendData = false;
-        }
-
-        // Make sure the 2 handshake start
-        if (hasReceivedHelloPacket == false) {
-          Serial.write((uint8_t*) &hello_packet_response, sizeof(hello_packet_response));  
-          hasReceivedHelloPacket = true;  
-        } else if (hasReceivedConnPacket == false) {
-          Serial.write((uint8_t*) &hello_packet_response, sizeof(hello_packet_response));  
-          hasReceivedConnPacket = true;
-          isReadyToSendData = true; 
-        }
-      } 
-
-
-      for (int i = 0; i < 20; i += 1) {
-        incomingData[i] = -1;
-      }
-      
-      index = 0;
-    }
-  
-
-  // The part on sending data
-  if (isReadyToSendData) {
-    struct Data_Packet data_packet;
-    struct Glove_Data glove_data;
-
-    glove_data = getGloveReadings();
-
-    data_packet = computeDataPacketResponse(glove_data);
-    Serial.write((uint8_t*) &data_packet, sizeof(data_packet));
-  }
-  delay(10);
-} 
+   
